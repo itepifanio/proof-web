a85029282d1ac9923cc28114f70512a2ab2e97ce
Epitácio Bessa <bessa.epitacio@gmail.com>
2019-10-19 12:12:02 -0300

    Update c0n001.v

--- exercicios/c0n001.v
+++ exercicios/c0n001.v
@@ -1,9 +1,16 @@
 Require Import ProofWeb.
 
-Parameter A : Prop.
+Parameter A B : Prop.
 
-Theorem c0n000 : (A -> A).
+Theorem c0n001 : (A -> (B -> A)).
 Proof.
+(*! prop_proof *)
+
 imp_i h1.
+imp_e A.
+imp_i h2.
+imp_i h3.
 exact h1.
+exact h1.
+
 Qed.
